25d8a89b491511a8502dc618214bb4ac08c22bb7
foxmoth <foxmoth@rimbahouse.com>
2023-05-29 19:37:43 +0700

    first commit

---
+++ resources/views/layouts/partials/navigation.blade.php
@@ -0,0 +1,22 @@
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">
+                <span class="d-block d-lg-none">Clarence Taylor</span>
+                <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="webassets/assets/img/profile.jpg" alt="..." /></span>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">Home</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">About us</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Visi Misi</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Profile</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#interests">Our Products & Services</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#awards">Gallery</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#awards">Our Clients</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#awards">Contact us</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#awards">Articles</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#awards">Events</a></li>
+                </ul>
+            </div>
+        </nav>
